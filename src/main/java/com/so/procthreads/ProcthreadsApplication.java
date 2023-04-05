196db3bb3485b55416fc8dbf63cf76ddeea38430
Thomas Hammes <thomas.hammes@hotmail.com>
2023-04-04 21:06:19 -0300

    Initial commit

---
+++ src/main/java/com/so/procthreads/ProcthreadsApplication.java
@@ -0,0 +1,13 @@
+package com.so.procthreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcthreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcthreadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/so/procthreads/ProcthreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.so.procthreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcthreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
